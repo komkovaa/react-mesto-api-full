ed1722d4eff1bf9635930a10f90a3b5704087f10
komkova <komkova.ar@gmail.com>
2022-12-04 15:27:56 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const http2 = require('node:http2');
+const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(requestLogger);
 
