5be4c00c9fb12450b0288b3d6e13b3871be1dc2a
komkova <komkova.ar@gmail.com>
2022-12-05 10:36:18 +0300

    меняете во фронтенде url бэкенда на тот, который будет на сервере

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'https://auth.nomoreparties.co';
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://komkova.students.nomoredomains.club';
 
 const request = ({
     url,
