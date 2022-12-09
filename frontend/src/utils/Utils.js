6945193a95f807e20f17687f4cfae12c4ce5670f
komkova <komkova.ar@gmail.com>
2022-12-09 23:41:09 +0300

    fix: удалить слеш в url

--- frontend/src/utils/Utils.js
+++ frontend/src/utils/Utils.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'https://api.komkova.students.nomoredomains.club/',
+    baseUrl: 'https://api.komkova.students.nomoredomains.club',
     headers:
     {
         'Authorization': '',
